5ca52a8b3035f605ee381789d46a7985cac23985
Jack Garabedian <33696930+jgarabedian@users.noreply.github.com>
2019-10-12 11:32:12 -0400

    Add files via upload
    Initial upload

---
+++ assets/js/components/header.js
@@ -0,0 +1,9 @@
+let headerHTML = '<a href="index.html" class="logo">';
+headerHTML += '<strong>Jack</strong>';
+headerHTML += '<span>by the man himself</span>';
+headerHTML += '</a>';
+headerHTML += '<nav>';
+headerHTML += '<a href="#menu">Menu</a>';
+headerHTML += '</nav>';
+
+document.getElementById('header').innerHTML = headerHTML;
