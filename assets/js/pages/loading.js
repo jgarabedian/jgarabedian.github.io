50dba2b6d1e3da9ec96f7c6e2caacf05e5420a8b
Jack Garabedian <33696930+jgarabedian@users.noreply.github.com>
2019-10-20 10:00:15 -0400

    add images and update movie

---
+++ assets/js/pages/loading.js
@@ -0,0 +1,7 @@
+// show a loading gif before iframe initializes
+function hideLoading() {
+    document.getElementById('iframeLoading').style.display = "none";
+    document.getElementById('iframeHolder').style.display = "block";
+}
+
+//
