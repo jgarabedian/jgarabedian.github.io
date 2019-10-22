a87ffe12d68d5ecdbc0143a3c4f136b9774f08fa
Jack Garabedian <33696930+jgarabedian@users.noreply.github.com>
2019-10-21 22:46:35 -0400

    show second page in index, menu, and why

--- assets/js/components/menu.js
+++ assets/js/components/menu.js
@@ -5,5 +5,6 @@ menuHTML += '<li><a href="/why.html">But Why?</a></li>';
 menuHTML += '<li><a href="/pages/visualizations/first.html">My First Viz</a></li>';
 menuHTML += '<li><a href="/pages/dashboards/movierecommendation.html">Movie Recommendations</a></li>';
 menuHTML += '<li><a href="/pages/notepad.html">Notepad++</a></li>';
+menuHTML += '<li><a href="/pages/visualizations/second.html">My Second Viz</a></li>';
 
 document.getElementById('menu-links').innerHTML = menuHTML;
