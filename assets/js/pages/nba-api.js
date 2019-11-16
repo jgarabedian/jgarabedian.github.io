51a6f4995fe0e8a056f54d07f3a117e05bd268ad
Jack Garabedian <33696930+jgarabedian@users.noreply.github.com>
2019-11-16 12:13:53 -0500

    create card in loop. TODO: make more modular

--- assets/js/pages/nba-api.js
+++ assets/js/pages/nba-api.js
@@ -3,13 +3,41 @@ results = {};
 // where do I want to return the results to?
 function getPlayers(results) {
     return results.map(function (player) {
-        const ul = document.getElementById('results');
-        let li = createElement('li');
-        let playerCard = `${player.first_name}` + ' ' + `${player.last_name}`;
-        playerCard += '<br>Team: ' + `${player.team.full_name}`;
-        li.innerHTML = playerCard;
-        appendElement(ul, li)
+        // const ul = document.getElementById('results');
+        // let li = createElement('li');
+        // let playerCard = `${player.first_name}` + ' ' + `${player.last_name}`;
+        // playerCard += '<br>Team: ' + `${player.team.full_name}`;
+        // playerCard += '<br>id: ' + `${player.id}`;
+        // li.innerHTML = playerCard;
+        // appendElement(ul, li)
+        // Use the logic to create cards
+        const cardContainer = document.getElementById('card-container');
+        let card = createElement('div');
+        addClass(card, 'player__card')
+        appendElement(cardContainer, card)
+        let cardHeader = createElement('div');
+        addClass(cardHeader, 'player__card__header')
+        appendElement(card, cardHeader);
+        let cardTeam = createElement('div');
+        addClass(cardTeam, 'player__card-team');
+        appendElement(cardHeader, cardTeam);
+        let h5 = createElement('h5');
+        appendElement(cardTeam, h5);
+        let team = `${player.team.full_name}`;
+        h5.innerHTML = team;
+        
     })
+}
+
+function addClass(el, className) {
+    el.className = className;
+    return el;
+}
+
+function createCard(el, player) {
+    el.className = 'player__card';
+    el.id = 'player__card-' + String(`${player.id}`);
+    return el
 }
 
 function createElement(el) {
@@ -39,15 +67,12 @@ function searchPlayer(search) {
         .then(function (response) {
             response.json().then(function (data) {
                 // results = data.data;
-                // console.log(data.data);
+                console.log(data.data);
                 getPlayers(data.data);
             }).catch(err => {
                 console.log(err);
             });
         })
-        // .then(result => {
-        //     console.log(result.data)
-        // })
         .catch(err => {
             console.log(err);
         });
@@ -59,8 +84,14 @@ window.onload = function () {
 }
 
 function getEventListeners() {
-        document.getElementById('playerSearchBtn').addEventListener('click', function () {
+    document.getElementById('playerSearchBtn').addEventListener('click', function () {
         var searchStr = document.getElementById('player-search-field').value;
         searchPlayer(searchStr);
+    });
+    document.getElementById('player-search-field').addEventListener('keypress', function (e) {
+        if (e.which === 13) {
+            var searchStr = document.getElementById('player-search-field').value;
+            searchPlayer(searchStr);
+        }
     })
 }
